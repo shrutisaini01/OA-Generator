66387fcafc40a27ea6f2860c2a3b7b928f3c32ff
Shruti Saini <141139555+shrutisaini01@users.noreply.github.com>
2025-08-05 21:17:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = 8000 || process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
 // ✅ MongoDB
